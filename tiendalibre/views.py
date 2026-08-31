d5381791b484330f9ea001408f428ad8c36fe97d
Smanfredini <santinomanfredini946@gmail.com>
2026-08-31 10:07:47 -0300

    fix: template dinámico con DTL en home

--- tiendalibre/views.py
+++ tiendalibre/views.py
@@ -14,7 +14,7 @@ def home(request):
         {"nombre": "Pantalón", "precio": 25000,"stock": 4,},
         {"nombre": "Zapatillas", "precio": 50000,"stock": 8,},
         {"nombre": "Campera", "precio": 70000,"stock": 2,},
-        {"nombre": "Gorra", "precio": 10000,"stock": 15,},
+        {"nombre": "Gorra", "precio": None ,"stock": 15,},
         { "nombre": "Mochila", "precio": 30000,"stock": 0,}
     ]
     return render(request, "tiendalibre/home.html", {"productos_destacados": productos_destacados})
